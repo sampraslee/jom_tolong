8f31b1e6694bd84b5038efbb4e3be46c5e86ca5c
sampraslee <sampras.lee8@gmail.com>
2020-04-04 20:22:06 +0800

    added code for verification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_062941) do
+ActiveRecord::Schema.define(version: 2020_04_04_122107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_062941) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "verified"
   end
 
   add_foreign_key "helps", "districts"
