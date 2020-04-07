a00c1ca08e39c8ef4cf23d865464cdac78c2a998
sampraslee <sampras.lee8@gmail.com>
2020-04-07 21:16:24 +0800

    added a filter system to filter users by help type, AJAX was used to render the relevant partial after filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
     def index
+        @users =
+        if params[:helps_search]
+            User.joins(:helps).where(helps: { help_type: params[:helps_search] })
+        else
+            User.all
+        end
+
+        p @users
+        
         @initiatives = Initiative.all
-        @users = User.all
     end
     
 end
