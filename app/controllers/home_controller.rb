488c63576436578d6b5986b618309eaf8d283745
sampraslee <sampras.lee8@gmail.com>
2020-04-08 21:30:08 +0800

    refactored queries to search through the helps table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
     def index
         byebug
-        @users =
+        @helps =
         if (params[:helps_search] != "" && params[:helps_search] != nil) && (params[:districts_search] != "" && params[:districts_search] != nil)
-            User.joins(:helps).where(helps: {help_type: params[:helps_search]} && {district_id: params[:districts_search]})
-        elsif params[:helps_search] || params[:districts_search]
-            User.joins(:helps).where(helps: { help_type: params[:helps_search]} || {district_id: params[:districts_search]})  
+            Help.where({help_type: params[:helps_search]} && {district_id: params[:districts_search]})
+        elsif (params[:helps_search] != "") && (params[:districts_search] == "")
+            Help.where({help_type: params[:helps_search]})
+        elsif (params[:helps_search] == "") && (params[:districts_search] != "")
+            Help.where({district_id: params[:districts_search]})
         else
-            User.all
+            Help.all
         end
-        
+
         @initiatives = Initiative.all
     end
     
