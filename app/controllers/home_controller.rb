3e177f1faffccbcdc26c2049c9d4353858083cc8
sampraslee <sampras.lee8@gmail.com>
2020-04-08 12:39:29 +0800

    added conditionals to the queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
     def index
+        byebug
         @users =
-        if params[:helps_search] && params[:districts_search]
+        if (params[:helps_search] != "" && params[:helps_search != nil]) && (params[:districts_search] != "" && params[:districts_search] != nil)
             User.joins(:helps).where(helps: {help_type: params[:helps_search]} && {district_id: params[:districts_search]})
         elsif params[:helps_search] || params[:districts_search]
             User.joins(:helps).where(helps: { help_type: params[:helps_search]} || {district_id: params[:districts_search]})  
