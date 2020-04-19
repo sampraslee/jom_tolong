7f903d4d3a8b5df0ae0424470bcb19f3c6c68f2c
sampraslee <sampras.lee8@gmail.com>
2020-04-19 22:07:18 +0800

    added javascript to show areas based on state selected

---
+++ app/javascript/packs/new.js
@@ -0,0 +1,26 @@
+const states_field = document.getElementById('user_helps_attributes_0_districts_state_id');
+const areas_field = document.getElementById('user_helps_attributes_0_district_id');
+
+states_field.addEventListener('change', filterArea);
+
+function filterArea() {
+	console.log('working');
+
+	// 1. get the state selected by user
+	const selectedState = states_field.value;
+	console.log(selectedState);
+
+	// 2. go through all the states within the areas field
+	const areasStates = areas_field.getElementsByTagName('optgroup');
+	for (let i = 0; i < areasStates.length; i++) {
+		const areaState = areasStates[i];
+		console.log(areaState);
+		// 3. check if the selected state equals to any state in the areas field
+		if (selectedState === areaState.attributes.label.value) {
+			console.log(selectedState === areaState.attributes.label.value);
+
+			// 4. Get the areas associated with the states
+			areas_field.innerHTML = areaState.innerHTML;
+		}
+	}
+}
