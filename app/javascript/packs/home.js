84c55ec2c974412529074c2aa253cf0420458419
sampraslee <sampras.lee8@gmail.com>
2020-04-20 13:40:04 +0800

    added the javascript filter to home page filter section

---
+++ app/javascript/packs/home.js
@@ -0,0 +1,23 @@
+const states_field = document.getElementById('state_search');
+const areas_field = document.getElementById('districts_search');
+
+states_field.addEventListener('change', filterArea);
+
+function filterArea() {
+	console.log('working');
+
+	const selectedState = states_field.value;
+	console.log(selectedState);
+
+	const areasStates = areas_field.getElementsByTagName('optgroup');
+	for (let i = 0; i < areasStates.length; i++) {
+		const areaState = areasStates[i];
+		console.log(areaState);
+		if (selectedState != areaState.attributes.label.value) {
+			console.log(selectedState === areaState.attributes.label.value);
+			areaState.hidden = true;
+		} else {
+			areaState.hidden = false;
+		}
+	}
+}
