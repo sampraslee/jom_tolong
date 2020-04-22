7d19977b025cb16047e94efa99fc7385cbeabbae
sampraslee <sampras.lee8@gmail.com>
2020-04-22 20:12:35 +0800

    used jQuery to hide optgroups, still doesnt work on safari.

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,23 +1,28 @@
-const states_field = document.getElementById('state_search');
-const areas_field = document.getElementById('districts_search');
+// const states_field = document.getElementById('state_search');
+// const areas_field = document.getElementById('districts_search');
+var states_field = $('select#state_search');
+var areas_field = $('select#districts_search');
 
-states_field.addEventListener('change', filterArea);
+// states_field.addEventListener('change', filterArea);
+states_field.change(filterArea);
 
 function filterArea() {
 	console.log('working');
 
-	const selectedState = states_field.value;
+	var selectedState = $(states_field).find(':selected').text();
 	console.log(selectedState);
 
-	const areasStates = areas_field.getElementsByTagName('optgroup');
+	// const areasStates = areas_field.getElementsByTagName('optgroup');
+	var areasStates = $('select#districts_search optgroup');
+
 	for (let i = 0; i < areasStates.length; i++) {
 		const areaState = areasStates[i];
 		console.log(areaState);
 		if (selectedState != areaState.attributes.label.value) {
 			console.log(selectedState === areaState.attributes.label.value);
-			areaState.hidden = true;
+			$(areaState).hide();
 		} else {
-			areaState.hidden = false;
+			$(areaState).show();
 		}
 	}
 }
